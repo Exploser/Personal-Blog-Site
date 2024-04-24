2ea9679b695177a9b2c92876d62a5c92d4832f5d
Exploser <exploserbusiness@gmail.com>
2024-04-24 17:20:34 -0400

    Improve handling of userinfo for better security

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
 
 export default function Header() {
-  const [username, setUsername] = useState('');
+  const { setUserInfo, userInfo } = useContext(UserContext);
+
   useEffect(() => {
     fetch('http://localhost:4000/profile', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
-        setUsername(userInfo.username);
+        setUserInfo(userInfo);
       });
     });
 
@@ -19,8 +21,9 @@ export default function Header() {
       credentials: 'include',
       method: 'POST',
     });
-    setUsername(null);
+    setUserInfo(null);
   }
+  const username = userInfo?.username;
 
   return (
     <header>
