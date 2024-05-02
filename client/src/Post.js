bfa46136ec5949d94586c76a5029c0bbac8c4416
Exploser <exploserbusiness@gmail.com>
2024-05-02 13:46:41 -0400

    Add Spinner to display loading of the posts

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,5 +1,7 @@
 import { formatISO9075 } from "date-fns";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from "react";
 
 function convertToFirebaseUrl(storageUrl) {
   const matches = storageUrl.match(/https:\/\/storage\.googleapis\.com\/([^\/]+)\/(.+)/);
@@ -13,11 +15,29 @@ function convertToFirebaseUrl(storageUrl) {
 
 export default function Post({ _id, title, description, cover, createdAt, author }) {
   const convertedCover = convertToFirebaseUrl(cover);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const image = new Image();
+    image.src = convertedCover;
+    image.onload = () => setIsLoading(false); // set loading to false when image is loaded
+  }, [convertedCover]);
+
+  if (isLoading) {
+    return (
+      <div className='text-center'>
+        <div className='spinner-border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className='post'>
       <div className='images'>
         <Link to={`/post/${_id}`}>
-          <img src={`${convertedCover}`} alt="Blog"></img>
+          <img src={convertedCover} alt="Blog"></img>
         </Link>
       </div>
       <div className='texts'>
@@ -25,7 +45,7 @@ export default function Post({ _id, title, description, cover, createdAt, author
           <h2>{title}</h2>
         </Link>
         <p className='info'>
-          <p className='author'>{author.username}</p>
+          <span className='author'>{author.username}</span>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className='description'>{description}</p>
@@ -33,3 +53,4 @@ export default function Post({ _id, title, description, cover, createdAt, author
     </div>
   );
 }
+
